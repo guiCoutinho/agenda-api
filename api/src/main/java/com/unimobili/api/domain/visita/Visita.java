3a024d649377ce2c976c14ca28cfdad29f4373e0
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2026-02-13 13:56:49 -0300

    feat: commit entity

---
+++ api/src/main/java/com/unimobili/api/domain/visita/Visita.java
@@ -0,0 +1,47 @@
+package com.unimobili.api.domain.visita;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Table(name = "visita")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Visita {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private OffsetDateTime criado_em;
+
+    private OffsetDateTime data_hora;
+
+    private String criado_por;
+
+    private String designado_a;
+
+    private String nome_cliente;
+
+    private String telefone_cliente;
+
+    private String chaves;
+
+    private String observacoes;
+
+    private String status;
+
+    private String duracao;
+
+    private Boolean ativa;
+}
