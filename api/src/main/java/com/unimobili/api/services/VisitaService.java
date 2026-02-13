7af5737a8f393bd4ef0c2b4d225ac4bcbfd5bb10
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2026-02-13 15:51:19 -0300

    feat: Padroniza datetime

--- api/src/main/java/com/unimobili/api/services/VisitaService.java
+++ api/src/main/java/com/unimobili/api/services/VisitaService.java
@@ -5,13 +5,14 @@ import com.unimobili.api.domain.visita.VisitaRequestDTO;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class VisitaService {
     public Visita createVisita(VisitaRequestDTO data){
         Visita novaVisita = new Visita();
         novaVisita.setCriado_em(OffsetDateTime.now());
-        novaVisita.setData_hora(OffsetDateTime.parse(data.data_hora()));
+        novaVisita.setData_hora(OffsetDateTime.parse(data.data_hora()).withOffsetSameInstant(ZoneOffset.UTC));
         novaVisita.setCriado_por(data.criado_por());
         novaVisita.setDesignado_a(data.designado_a());
         novaVisita.setNome_cliente(data.nome_cliente());
