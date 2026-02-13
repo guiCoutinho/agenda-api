9a185c9bb507c5396f2b6da9b0774b442d145c97
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2026-02-13 15:45:37 -0300

    feat: VisitaController atualizada, api funcionando ate o momento

--- api/src/main/java/com/unimobili/api/controller/VisitaController.java
+++ api/src/main/java/com/unimobili/api/controller/VisitaController.java
@@ -5,6 +5,7 @@ import com.unimobili.api.domain.visita.VisitaRequestDTO;
 import com.unimobili.api.services.VisitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class VisitaController {
     @Autowired
     private VisitaService visitaService;
 
+    @PostMapping
     public ResponseEntity<Visita> create(@RequestBody VisitaRequestDTO body) {
         Visita novaVisita = this.visitaService.createVisita(body);
         return ResponseEntity.ok(novaVisita);
