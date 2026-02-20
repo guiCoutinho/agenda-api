c10af8cae5e35391b011a0db4864eb514a69eaa9
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2026-02-20 13:45:34 -0300

    Altera o tipo de status

--- api/src/main/java/com/unimobili/api/repositories/VisitaRepository.java
+++ api/src/main/java/com/unimobili/api/repositories/VisitaRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.OffsetDateTime;
-import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 public interface VisitaRepository extends JpaRepository<Visita, UUID> {
