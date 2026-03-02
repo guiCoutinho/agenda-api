7302b4323c523c5dc1cc448b3156aa762354a0c9
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2026-03-02 12:28:20 -0300

    Cria table users e autenticacao com security

---
+++ api/src/main/java/com/unimobili/api/domain/enums/UserRoles.java
@@ -0,0 +1,17 @@
+package com.unimobili.api.domain.enums;
+
+public enum UserRoles {
+    ADMIN('admin'),
+    ATENDENTE('atendente'),
+    VISITADOR('visitador');
+
+    private String role;
+
+    UserRoles(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+}
