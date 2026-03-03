c4543318852e0246a9596b986e5d2a3ef3125ed6
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2026-03-03 12:20:17 -0300

    corrige migration

--- api/src/main/resources/db/migration/V1__create-visita-table.sql
+++ api/src/main/resources/db/migration/V1__create-visita-table.sql
@@ -4,7 +4,7 @@ CREATE TABLE visita (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     criado_em TIMESTAMP WITH TIME ZONE,
     data_hora TIMESTAMP WITH TIME ZONE,
-    criado_por_id ,
+    criado_por VARCHAR(100) NOT NULL,
     designado_a VARCHAR(100) NOT NULL,
     nome_cliente VARCHAR(100) NOT NULL,
     telefone_cliente VARCHAR(30) NOT NULL,
